02b880c7d1bc7c5173ecd8cbdb17dd58603f874b
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-24 17:20:26 +0200

    Update docker_master.pp

--- site-modules/profile/manifests/docker/docker_master.pp
+++ site-modules/profile/manifests/docker/docker_master.pp
@@ -22,11 +22,48 @@ class profile::docker::docker_master {
 				}
 		
 		
-		
-	docker::registry {'https://hub.docker.com/_/wordpress':
-  username => 'username',
-  password => 'password',
-}
+		class {'docker::compose':
+	  ensure => present,
+	}
+
+	file { '/tmp/docker-compose.yml':
+		ensure  => present,
+		content => "
+version: '3.3'
+
+services:
+   db:
+     image: mysql:5.7
+     volumes:
+       - db_data:/var/lib/mysql
+     restart: always
+     environment:
+       MYSQL_ROOT_PASSWORD: somewordpress
+       MYSQL_DATABASE: wordpress
+       MYSQL_USER: wordpress
+       MYSQL_PASSWORD: wordpress
+
+   wordpress:
+     depends_on:
+       - db
+     image: wordpress:latest
+     ports:
+       - \"80:80\"
+     restart: always
+     environment:
+       WORDPRESS_DB_HOST: db:3306
+       WORDPRESS_DB_USER: wordpress
+       WORDPRESS_DB_PASSWORD: wordpress
+       WORDPRESS_DB_NAME: wordpress
+volumes:
+    db_data: {}"
+	}
+	
+	docker_compose { 'test':
+		compose_files => ['/tmp/docker-compose.yml'],
+		ensure  => present,
+		require => [Class['docker'], File['/tmp/docker-compose.yml'], ], 
+	}
 }
 
 
