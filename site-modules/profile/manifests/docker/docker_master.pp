f74ecc613c2993d58fffe09d631f1582334cac19
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-24 23:10:19 +0200

    Update docker_master.pp

--- site-modules/profile/manifests/docker/docker_master.pp
+++ site-modules/profile/manifests/docker/docker_master.pp
@@ -32,32 +32,19 @@ class profile::docker::docker_master {
 version: '3.3'
 
 services:
-   db:
-     image: mysql:5.7
-     volumes:
-       - db_data:/var/lib/mysql
-     restart: always
-     environment:
-       MYSQL_ROOT_PASSWORD: somewordpress
-       MYSQL_DATABASE: wordpress
-       MYSQL_USER: wordpress
-       MYSQL_PASSWORD: wordpress
-     deploy:
-       replicas: 1
    wordpress:
      depends_on:
        - db
      image: wordpress:latest
      ports:
        - \"80:80\"
-     restart: always
      environment:
        WORDPRESS_DB_HOST: db:3306
        WORDPRESS_DB_USER: wordpress
        WORDPRESS_DB_PASSWORD: wordpress
        WORDPRESS_DB_NAME: wordpress
      deploy:
-       replicas: 1
+       replicas: 4
 volumes:
     db_data: {}"
 	
