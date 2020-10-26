cd2984ed156779475a56f08bec0a88b6f7726010
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-26 16:59:41 +0100

    Update database_server.pp

--- site-modules/profile/manifests/database/database_server.pp
+++ site-modules/profile/manifests/database/database_server.pp
@@ -6,7 +6,7 @@ include '::mysql::server'
 	mysql::db { 'wordpress':
 	  user     => 'wordpress',
 	  password => 'wordpress',
-	  host     => facts['networking']['ip'],
+	  host     => $facts['networking']['ip'],
 	  grant    => ['SELECT', 'UPDATE'],
 	}
 
