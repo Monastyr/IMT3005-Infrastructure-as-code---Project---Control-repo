fb910b67553db6eeaa415fa425cb57c166d724d6
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-27 18:53:55 +0100

    Update wordpress_server.pp

--- site-modules/role/manifests/wordpress_server.pp
+++ site-modules/role/manifests/wordpress_server.pp
@@ -2,6 +2,6 @@ class role::wordpress_server{
   include ::profile::base_linux
   include ::profile::dns::client
   include ::profile::consul::client
-  include ::profile::wordpress
+  #include ::profile::wordpress
   include ::profile::docker::docker_worker
   }
