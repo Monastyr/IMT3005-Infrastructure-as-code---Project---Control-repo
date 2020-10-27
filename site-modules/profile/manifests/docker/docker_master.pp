dbfbecf487905f326c4bcdc341d8ffffbe23c119
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-27 01:08:52 +0100

    Update docker_master.pp

--- site-modules/profile/manifests/docker/docker_master.pp
+++ site-modules/profile/manifests/docker/docker_master.pp
@@ -80,12 +80,12 @@ volumes:
 	}
 	
 	
-	node 'manager.node.consul' {
+	
   # Configure puppetdb and its underlying database
   class { 'puppetdb': }
   # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
-		}
+		
 	
 	
 	
