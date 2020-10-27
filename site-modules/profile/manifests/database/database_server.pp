b86f143f779327fe161ff326f6342c641fd49376
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-27 17:43:52 +0100

    Update database_server.pp

--- site-modules/profile/manifests/database/database_server.pp
+++ site-modules/profile/manifests/database/database_server.pp
@@ -4,14 +4,9 @@ class profile::database::database_server{
 class { '::mysql::server':
   root_password           => 'password',
   restart                		  => true,
-  override_options        => $override_options
+  override_options => { 'mysqld' => { 'bind_address' => 'db.node.consul' } }
 }
 
-
-$override_options = {
-  'mysqld' => {
-    'bind-address' => 'db.node.consul',
-  }
 }
 mysql::db { 'wordpress':
   user     => 'wordpress',
