156c10bc7ad272f5b04b87f976155050838ce8d2
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-27 16:53:51 +0100

    Update wordpress.pp

--- site-modules/profile/manifests/wordpress.pp
+++ site-modules/profile/manifests/wordpress.pp
@@ -3,7 +3,6 @@ class profile::wordpress {
 $mysql_password        = 'password'
 $wordpress_password    = 'password'
 
-  # PHP-enabled web server
   class { 'apache':
     default_vhost => false,
     mpm_module    => 'prefork',
@@ -19,28 +18,20 @@ $wordpress_password    = 'password'
     manage_docroot => false,
   }
 
-  # MySQL server
-  class { 'mysql::server':
-    root_password => $mysql_password,
-  }
-  class { 'mysql::bindings':  # if added late, need to restart apache service
-    php_enable => true,
-    php_package_name => 'php-mysql',
-  }
 
   # Wordpress
   user { 'wordpress':
     ensure => 'present'
   }
   class { 'wordpress':
-    version     => '4.9.7',   # if changed, delete index.php, rerun puppet
-                              # due to creates => "${install_dir}/index.php"
-                              # in manifests/instance/app.pp
+    version     => '4.9.7',   	# if changed, delete index.php, rerun puppet
+										# due to creates => "${install_dir}/index.php"
+										# in manifests/instance/app.pp
     wp_owner    => 'wordpress',
     wp_group    => 'wordpress',
     db_user     => 'wordpress',
     db_password => $wordpress_password,
-    require     => [ Class['apache'], Class['mysql::server'], User['wordpress'] ],
+    require     => [ Class['apache'], Class['::profile::database::database_server::mysql::server'], User['wordpress'] ],
   }
 
 }
