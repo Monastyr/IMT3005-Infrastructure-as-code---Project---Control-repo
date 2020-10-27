b8d86ac4f820e42741e1df2c75f2eb4c512afef5
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-27 11:14:14 +0100

    Update database_server.pp

--- site-modules/profile/manifests/database/database_server.pp
+++ site-modules/profile/manifests/database/database_server.pp
@@ -3,10 +3,10 @@ class profile::database::database_server{
 
 include '::mysql::server'
 
-mysql::db { 'mydb':
-  user     => 'myuser',
-  password => 'mypass',
-  host     => 'localhost',
+mysql::db { 'wordpress':
+  user     => 'wordpress',
+  password => 'wordpress',
+  host     => 'db.node.consul',
   grant    => ['SELECT', 'UPDATE'],
 }
 
