e5c825234c8dc11159a47a81cc5cb59d0d6b155e
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-27 20:26:58 +0100

    a

--- site-modules/profile/manifests/database/database_server.pp
+++ site-modules/profile/manifests/database/database_server.pp
@@ -3,7 +3,29 @@ class profile::database::database_server{
 
 class { '::mysql::server':
   root_password           => 'password',
+  override_options        => $override_options,
   }
 
 
+
+mysql::db {'wordpress':
+	user 			=> 'boss',
+	password 	=> 'boss',
+	dbname 	=> 'wordpress',
+	host 			=> ${fqdn},
+	grant 		=> ['ALL PRIVILEGES'],
+}
+mysql_user { 'boss@%':
+  ensure                   => 'present',
+  password_hash => mysql_password('password'),
+} ->	
+
+
+mysql_grant { 'boss@%/*.*':
+ ensure     => 'present',
+ options    => ['GRANT'],
+ privileges => ['ALL PRIVILEGES'],
+ table      => '*.*',
+ user       => 'user@%',
+}
 }
