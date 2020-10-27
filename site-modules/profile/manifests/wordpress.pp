f9932c1e8e638e220738ecc1f675ac0336fbe17a
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-27 16:18:57 +0100

    Update wordpress.pp

--- site-modules/profile/manifests/wordpress.pp
+++ site-modules/profile/manifests/wordpress.pp
@@ -1,46 +1,7 @@
 class profile::wordpress { 
 	
-	
-	$mysql_password = 'put_in_hiera'
-	$wordpress_password = 'put_also_in_hiera'
-	
-	# PHP-enabled web server
-	class { 'apache':
-	default_vhost => false,
-	mpm_module => 'prefork',
-	}
-	class { 'apache::mod::php':
-	php_version => '7.2',
-	}
-	# Virtual Host for Wordpress
-	apache::vhost { $::fqdn:
-	port => '80',
-	docroot => '/opt/wordpress',
-	manage_docroot => false,
-	}
-	# MySQL server
-	class { 'mysql::server':
-	root_password => $mysql_password,
-	}
-	class { 'mysql::bindings': # if added late, need to restart apache service
-	php_enable => true,
-	php_package_name => 'php-mysql',
-	}
-	# Wordpress
-	user { 'wordpress':
-	ensure => 'present'
-	}
-	class { 'wordpress':
-	version => '4.9.7', # if changed, delete index.php, rerun puppet
-	# due to creates => "${install_dir}/index.php"
-	24
-	4.3. LAB TUTORIALS
-	# in manifests/instance/app.pp
-	wp_owner => 'wordpress',
-	wp_group => 'wordpress',
-	db_user => 'wordpress',
-	db_password => $wordpress_password,
-	require => [ Class['apache'], Class['mysql::server'], User['wordpress'] ],
+class { 'wordpress': }
+
 }
 
 	
