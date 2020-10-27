5a50889adb98896d4fe65498616a9b1ea5fbb96d
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-27 17:50:52 +0100

    Update database_server.pp

--- site-modules/profile/manifests/database/database_server.pp
+++ site-modules/profile/manifests/database/database_server.pp
@@ -1,13 +1,2 @@
 class profile::database::database_server{
-
-
-class { 'mysql::server':
-    root_password =>'password',
-  }
-  class { 'mysql::bindings':  # if added late, need to restart apache service
-    php_enable => true,
-    php_package_name => 'php-mysql',
-  }
-
-
 }
