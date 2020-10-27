bc9db1e823cca2aea777766e6331f3965a67326a
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-27 16:58:25 +0100

    Update wordpress.pp

--- site-modules/profile/manifests/wordpress.pp
+++ site-modules/profile/manifests/wordpress.pp
@@ -1,37 +1,20 @@
 class profile::wordpress { 
 	
-$mysql_password        = 'password'
-$wordpress_password    = 'password'
-
-  class { 'apache':
-    default_vhost => false,
-    mpm_module    => 'prefork',
-  }
-  class { 'apache::mod::php':
-      php_version => '7.2',
-  }
-
-  # Virtual Host for Wordpress
-  apache::vhost { $::fqdn:
-    port           => '80',
-    docroot        => '/opt/wordpress',
-    manage_docroot => false,
-  }
-
-
-  user { 'wordpress':
-    ensure => 'present'
-  }
-  class { 'wordpress':
-    version     => '4.9.7',   	# if changed, delete index.php, rerun puppet
-										# due to creates => "${install_dir}/index.php"
-										# in manifests/instance/app.pp
-    wp_owner    => 'wordpress',
-    wp_group    => 'wordpress',
-    db_user     => 'wordpress',
-    db_password => $wordpress_password,
-    require     => [ Class['apache'], Class['mysql::server'], User['wordpress'] ],
-  }
+	class { 'apache': }
+	
+	apache::vhost { 'wordpress.com':
+		port             => '80',
+	    docroot          => '/var/www/wordpress',
+		#fallbackresource => '/index.php',
+	}
+	
+	
+	class { 'wordpress':
+		db_user	=> 'wordpress',
+		db_password => 'wordpress',
+		db_host => 'db.node.consul',
+		install_dir => '/var/www/wordpress',
+	}
 
 }
 
