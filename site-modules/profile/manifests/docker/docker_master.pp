3961d8c7fd9c400724699a44c3b72aff905e4800
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-28 18:33:15 +0100

    Update docker_master.pp

--- site-modules/profile/manifests/docker/docker_master.pp
+++ site-modules/profile/manifests/docker/docker_master.pp
@@ -42,8 +42,8 @@ services:
      restart: always
      environment:
        WORDPRESS_DB_HOST: db.node.consul:3306
-       WORDPRESS_DB_USER: boss
-       WORDPRESS_DB_PASSWORD: boss
+       WORDPRESS_DB_USER: boss2
+       WORDPRESS_DB_PASSWORD: boss2
        WORDPRESS_DB_NAME: wordpress
      deploy:
       replicas: 3
