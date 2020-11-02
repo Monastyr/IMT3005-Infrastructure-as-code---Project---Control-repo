2a279500e8080a28a96fd521b15deaaec25acde2
Monastyr <wojtaszek1910rts@gmail.com>
2020-11-02 12:10:09 -0800

    gallera -

--- site-modules/profile/manifests/database/database_server.pp
+++ site-modules/profile/manifests/database/database_server.pp
@@ -1,28 +1,19 @@
 class profile::database::database_server{
 
 
-#class { '::mysql::server':
-#  root_password           => 'password',
-#  override_options => {'mysqld' => {'bind-address' => '0.0.0.0'}},
-#}
+class { '::mysql::server':
+  root_password           => 'password',
+  override_options => {'mysqld' => {'bind-address' => '0.0.0.0'}},
+  }
 
 
 
-#mysql::db {'wordpress':
-#	user 			=> 'boss',
-#	password 	=> 'boss',
-#	dbname 	=> 'wordpress',
-#	host 			=> '%',
-#	grant  		=> ['ALL PRIVILEGES'],
-#}
-
-
-class { 'galera':
-  cluster_name    => 'mycluster',
-  galera_servers  => ['db2.node.consul'],
-  galera_master   => 'db1.node.consul',
-  root_password   => 'password',
-  status_password => 'password',
+mysql::db {'wordpress':
+	user 			=> 'boss',
+	password 	=> 'boss',
+	dbname 	=> 'wordpress',
+	host 			=> '%',
+	grant  		=> ['ALL PRIVILEGES'],
 }
 
 #mysql_user { 'boss2@%':
