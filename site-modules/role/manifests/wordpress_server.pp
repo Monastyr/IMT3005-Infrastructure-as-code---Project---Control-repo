50fd99f591e478430c290a58ec9fd6a0a2644d00
Monastyr <wojtaszek1910rts@gmail.com>
2020-11-02 23:26:59 +0100

    Restoring a file that has been deleted by mistake

---
+++ site-modules/role/manifests/wordpress_server.pp
@@ -0,0 +1,7 @@
+class role::wordpress_server{
+  include ::profile::base_linux
+  include ::profile::dns::client
+  include ::profile::consul::client
+  #include ::profile::wordpress
+  include ::profile::docker::docker_worker
+  }
