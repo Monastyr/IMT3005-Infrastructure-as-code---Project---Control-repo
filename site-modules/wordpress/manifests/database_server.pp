563d5d1d450118993a61dc84eafb5f405acad449
Sander <sandeho@stud.ntnu.no>
2020-11-22 14:49:30 +0100

    Comments

--- site-modules/wordpress/manifests/database_server.pp
+++ site-modules/wordpress/manifests/database_server.pp
@@ -1,7 +1,8 @@
 # @summary A short summary of the purpose of this class
 #
-# A description of what this class does
-#
+# A description of what this class does:
+# To install a server with the default options
+# To create a database with a user and some assigned privileges
 # @example
 #   include wordpress::database_server
 class wordpress::database_server {
@@ -10,6 +11,7 @@ class { '::mysql::server':
   root_password    => 'password',
   override_options => {'mysqld' => {'bind-address' => '0.0.0.0'}},
   }
+
 mysql::db {'wordpress':
 	user 			=> 'boss',
 	password 	=> 'boss',
