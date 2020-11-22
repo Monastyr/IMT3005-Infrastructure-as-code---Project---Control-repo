faec7430f69909c29fe179762e240c551e55a70c
Sander <sandeho@stud.ntnu.no>
2020-11-22 13:50:11 +0100

    Updated conf.pp

--- site-modules/wordpress/manifests/conf.pp
+++ site-modules/wordpress/manifests/conf.pp
@@ -5,4 +5,21 @@
 # @example
 #   include wordpress::conf
 class wordpress::conf {
+    # You can change the values of these variables
+    # according to your preferences
+
+
+    $volumepath = '/var/www/html/wp-content'
+    $db_name = 'wordpress'
+    $db_user = 'boss'
+    $db_user_password = 'boss'
+    $db_host = 'db1.node.consul'
+
+    # Don't change the following variables
+
+    # This will evaluate to wp@localhost
+    #$db_user_host = "${db_user}@${db_host}"
+
+    # This will evaluate to wp@localhost/wordpress.*
+    #$db_user_host_db = "${db_user}@${db_host}/${db_name}.*"
 }
