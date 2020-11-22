9e712a972384857d1cde23ccac86c9f6e95608f2
Sander <sandeho@stud.ntnu.no>
2020-11-22 13:08:32 +0100

    mysql server and db

--- site-modules/wordpress/manifests/database_server.pp
+++ site-modules/wordpress/manifests/database_server.pp
@@ -5,4 +5,16 @@
 # @example
 #   include wordpress::database_server
 class wordpress::database_server {
+
+class { '::mysql::server':
+  root_password           => 'password',
+  override_options => {'mysqld' => {'bind-address' => '0.0.0.0'}},
+  }
+mysql::db {'wordpress':
+	user 			=> 'boss',
+	password 	=> 'boss',
+	dbname 	=> 'wordpress',
+	host 			=> '%',
+  grant  		=> ['ALL PRIVILEGES'],
+
 }
