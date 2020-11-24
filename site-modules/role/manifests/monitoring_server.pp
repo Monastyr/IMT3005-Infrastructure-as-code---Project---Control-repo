5a30c7ed99adfd1c31064565857bdb4a5c9407a3
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-11-24 17:26:25 +0100

    Update monitoring_server.pp

--- site-modules/role/manifests/monitoring_server.pp
+++ site-modules/role/manifests/monitoring_server.pp
@@ -2,5 +2,6 @@ class role::monitoring_server {
   include ::profile::base_linux
   include ::profile::dns::client
   include ::profile::consul::server
+  include ::profile::sensu::backend
   include ::wordpress::monitor
 }
